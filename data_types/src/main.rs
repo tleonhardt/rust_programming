873c9e60d7613bd48c0bb30fdde7968adbb3c7fb
Todd Leonhardt <tleonha@amazon.com>
2022-02-15 22:47:25 -0500

    Finished data_types example

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,7 +1,11 @@
+use std::io;
+
 fn main() {
+    // Integers
     let guess: u32 = "42".parse().expect("Not a number!");
     println!("Guess is {}", guess);
 
+    // Floating point types
     let x = 2.0; // f64
     let y: f32 = 3.0; // f32
     println!("x = {}, y = {}", x, y);
@@ -21,4 +25,59 @@ fn main() {
 
     // remainder
     let remainder = 43 % 5;
+
+    // Booleans
+    let t = true;
+    let f: bool = false; // with explicit type annotation
+
+    // Character types
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+
+    // Tuples
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (t1, t2, t3) = tup;
+    println!("The value of t2 is: {}", t2);
+    let five_hundred = tup.0;
+
+    // Arrays
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    let first = a[0];
+    let second = a[1];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
 }
