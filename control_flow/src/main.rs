c1e2c176902ff19e614707e67179c9865252a0bd
Todd Leonhardt <tleonha@amazon.com>
2022-02-16 08:13:07 -0500

    Update control flow

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,9 +1,18 @@
 fn main() {
-    let number = 3;
+    let number = 6;
 
-    if number < 5 {
-        println!("condition was true");
+    if number % 4 == 0 {
+        println!("{} is divisible by 4", number);
+    } else if number % 3 == 0 {
+        println!("{} is divisible by 3", number);
+    } else if number % 2 == 0 {
+        println!("{} is divisible by 2", number);
     } else {
-        println!("condition was false");
+        println!("{} is not divisible by 4, 3, or 2", number);
     }
+
+    let even = number % 2 == 0;
+    let parity = if even { "even" } else { "odd" };
+
+    println!("{} is {}", number, parity);
 }
