2d81761a86cc0371ef25772a73e7f5d14f1e63ca
Todd Leonhardt <tleonha@amazon.com>
2022-02-16 22:37:01 -0500

    Returning from break

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,21 +1,13 @@
 fn main() {
-    let mut count = 0;
-    'counting_up: loop {
-        println!("count = {}", count);
-        let mut remaining = 10;
+    let mut counter = 0;
 
-        loop {
-            println!("remaining = {}", remaining);
-            if remaining == 9 {
-                break;
-            }
-            if count == 2 {
-                break 'counting_up;
-            }
-            remaining -= 1;
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
         }
+    };
 
-        count += 1;
-    }
-    println!("End count = {}", count);
+    println!("The result is {}", result);
 }
