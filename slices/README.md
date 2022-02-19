7015ae8a3fde88a5aaa22150100c6921993f0e22
Todd Leonhardt <tleonha@amazon.com>
2022-02-19 14:49:05 -0500

    Added slices example

---
+++ slices/README.md
@@ -0,0 +1,3 @@
+# The Slice Type
+
+This code follows [Chapter 4.3](https://doc.rust-lang.org/nightly/book/ch04-03-slices.html) of the [The Rust Programming Language](https://doc.rust-lang.org/nightly/book) book.
---
+++ slices/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let s = String::from("hello world");
+
+    let hello = first_word(&s);
+
+    println!("{}", hello);
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
