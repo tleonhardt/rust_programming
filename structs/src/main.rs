f8d87ebbf7d4e850d58a392d44fe35a6f9ecbf4f
Todd Leonhardt <tleonha@amazon.com>
2022-02-19 16:23:53 -0500

    Updated structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -5,11 +5,37 @@ struct User {
     sign_in_count: u64,
 }
 
+// Tuple structs
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+// Unit-like struct
+struct AlwaysEqual;
+
 fn main() {
-    let user1 = User {
-        email: String::from("someone@example.com"),
-        username: String::from("someusername123"),
+    let mut user1 = build_user(
+        String::from("someone@example.com"),
+        String::from("someusername123"),
+    );
+    user1.email = String::from("anotheremail@example.com");
+
+    // The syntax .. specifies the remaining fields not explicitly set should have same values as user1
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    let subject = AlwaysEqual;
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
         active: true,
         sign_in_count: 1,
-    };
+    }
 }
