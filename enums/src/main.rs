6604ca0ebacd42a744a80feba1efa9e066a9cfc5
Todd Leonhardt <tleonha@amazon.com>
2022-02-20 13:12:25 -0500

    Update enums example

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,9 +1,27 @@
-enum IpAddrKind {
-    V4,
-    V6,
+#![warn(clippy::all, clippy::pedantic)]
+
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+    }
 }
 
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
