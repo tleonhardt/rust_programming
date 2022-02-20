eb35ec9e80cddb44f228a1bb2b4c9a2d9689d993
Todd Leonhardt <tleonha@amazon.com>
2022-02-20 13:24:25 -0500

    Added match control flow example for enums

---
+++ match_control/README.md
@@ -0,0 +1,3 @@
+# The match Control Flow Construct
+
+This code follows [Chapter 6.2](https://doc.rust-lang.org/nightly/book/ch06-02-match.html) of the [The Rust Programming Language](https://doc.rust-lang.org/nightly/book) book.
---
+++ match_control/src/main.rs
@@ -0,0 +1,21 @@
+#[derive(Debug)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn main() {
+    let my_coin = Coin::Dime;
+    println!("Your coin is worth {} cents", value_in_cents(my_coin));
+}
