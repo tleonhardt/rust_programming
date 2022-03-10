67c83ff1ffac96fad1e0b506732b0c3cbf61240a
Todd Leonhardt <tleonha@amazon.com>
2022-03-09 20:46:39 -0500

    Expanded recoverable_errors example

--- recoverable_errors/src/main.rs
+++ recoverable_errors/src/main.rs
@@ -1,5 +1,24 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
-    let f: u32 = File::open("hello.txt");
+    let filename = "hello.txt";
+    let f = File::open(filename);
+
+    let f = match f {
+        Ok(file) => {
+            println!("File {:?} opened successfully", filename);
+            file
+        }
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create(filename) {
+                Ok(fc) => {
+                    println!("File {:?} created successfully", filename);
+                    fc
+                }
+                Err(e) => panic!("Error creating the file {:?}: {:?}", filename, e),
+            },
+            other_error => panic!("Problem opening the file {:?}: {:?}", filename, other_error),
+        },
+    };
 }
