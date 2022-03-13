30b857fcbed5438047b0bb92e5c5dcfa4d42e662
Todd Leonhardt <tleonha@amazon.com>
2022-03-13 15:01:52 -0400

    Added lifetimes example

---
+++ lifetimes/README.md
@@ -0,0 +1,3 @@
+# Validating References with Lifetimes
+
+This code follows [Chapter 10.3](https://doc.rust-lang.org/nightly/book/ch10-03-lifetime-syntax.html) of the [The Rust Programming Language](https://doc.rust-lang.org/nightly/book) book.
---
+++ lifetimes/src/main.rs
@@ -0,0 +1,23 @@
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+}
