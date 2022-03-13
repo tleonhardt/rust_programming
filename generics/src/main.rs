bb0efa5d848b246adaa9e0df2dd698bfafdce80e
Todd Leonhardt <tleonha@amazon.com>
2022-03-13 13:07:14 -0400

    Fixed largest with trait bounds

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -15,8 +15,29 @@ impl Point<f32> {
     }
 }
 
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
 fn main() {
     let p = Point { x: 5, y: 10 };
-
     println!("p.x = {}", p.x());
+
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
