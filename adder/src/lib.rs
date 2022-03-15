4be15ed0c37dc14df5fecbd458656541a64ab9c9
Todd Leonhardt <tleonha@amazon.com>
2022-03-15 08:51:49 -0400

    Added failing test

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,8 +1,13 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
+    fn exploration() {
         let result = 2 + 2;
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
     }
 }
