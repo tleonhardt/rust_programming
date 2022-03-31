6fb9bf4020aa15e9afbd5beb502bb4521f1ce3fe
Todd Leonhardt <tleonha@amazon.com>
2022-03-31 18:20:01 -0400

    updated iterators example

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -91,4 +91,26 @@ mod tests {
 
         assert_eq!(total, 6);
     }
+
+    #[test]
+    fn calling_next_directly() {
+        let mut counter = Counter::new();
+
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+    }
+
+    #[test]
+    fn using_other_iterator_trait_methods() {
+        let sum: u32 = Counter::new()
+            .zip(Counter::new().skip(1))
+            .map(|(a, b)| a * b)
+            .filter(|x| x % 3 == 0)
+            .sum();
+        assert_eq!(18, sum);
+    }
 }
